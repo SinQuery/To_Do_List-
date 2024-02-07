0641cbefd77adf0f56802cf0e187a6ef3dcc78ea
ThatCodeGuySA <ThatCodeGuySA@gmail.com>
2024-02-07 17:40:46 +0200

    Created Bin dropdown and Recycle Bin

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-  function toggleDropdown() {
-    var dropdown = document.getElementById("myDropdown");
-    dropdown.style.display = (dropdown.style.display === "block") ? "none" : "block";
-  }
+function toggleDropdown() {
+  var dropdown = document.getElementById("myDropdown");
+  dropdown.style.display = (dropdown.style.display === "block") ? "none" : "block";
+}
 
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function(event) {
-    if (document.querySelector.onclick('.round-button')) {
-      var dropdown = document.getElementById("myDropdown");
-      if (dropdown.style.display === "block") {
-        dropdown.style.display = "none";
-      }
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (document.querySelector.onclick('.round-button')) {
+    var dropdown = document.getElementById("myDropdown");
+    if (dropdown.style.display === "block") {
+      dropdown.style.display = "none";
     }
   }
+}
 
 // document.getElementById('checkBox').addEventListener('click', () => console.log('I have been clicked.'));
 // let checkBox = document.getElementById('checkBox');
@@ -19,5 +19,16 @@
 
 // Function for the submit button
 function inputFunc() {
-   
+ 
+}
+
+//Navbar Menu 
+
+function toggleDropdown(menuButton) {
+  var dropdown = document.getElementById(menuButton);
+  if (dropdown.style.display === 'block') {
+      dropdown.style.display = 'none';
+  } else {
+      dropdown.style.display = 'block';
+  }
 }
